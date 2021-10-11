623f080dbde2c0fcdb511f7ded96b419c299e9eb
JoseCedeño <81100807+Jose-22-ced@users.noreply.github.com>
2021-10-11 14:35:52 -0500

    Envio

---
+++ src/app/modules/proyectos/proyectocreate/proyectocreate.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proyectocreate',
+  templateUrl: './proyectocreate.component.html',
+  styleUrls: ['./proyectocreate.component.scss']
+})
+export class ProyectocreateComponent implements OnInit {
+  public ista='assets/images/ISTA.png'
+  public secretaria='assets/images/Secretaria.png'  
+
+  constructor() { 
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+  
+  
+
+}
